66bfd56c453a3c83e4463fb40503407017571f14
akshatha-swamy <akshathaswamy3@gmail.com>
2023-09-29 22:49:26 +0530

    deleting files

---
+++ planning.js
@@ -0,0 +1,18 @@
+//we need a reusable carousal
+//->hero carousel
+//common carousel
+//cast carousel(small)
+
+//pages
+
+//home page
+//movie page
+//authentication page
+//razorpay payment gateway
+
+//to build
+
+//layouts
+//pages
+//routes
+//components
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        navCol:
+        
+
+        
+
+        {
+          50: '#edf1ff',
+          100: '#cfd6e5',
+          200: '#b1bace',
+          300: '#939fb9',
+          400: '#7584a4',
+          500: '#5b6a8a',
+          600: '#46536c',
+          700: '#313b4e',
+          800: '#1c2331',
+          900: '#040d17',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
