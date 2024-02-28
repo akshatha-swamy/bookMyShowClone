04c20e99c55ddff47137d52ee409f54605df07a1
akshatha-swamy <akshathaswamy3@gmail.com>
2024-02-28 11:36:11 +0530

    image

--- src/config/TempPosters.config.js
+++ src/config/TempPosters.config.js
@@ -1,48 +1,48 @@
 const PremierImages=[
     {
-        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC40LzEwICA1MTkuMksgVm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00330424-mccryrbvyf-portrait.jpg" ,
+        src:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/jawan-et00330424-1693892482.jpg" ,
         alt: "Jawan",
         title:"Jawan",
         subtitle:"Action, Thriller"
     },
     {
-        src: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC45LzEwICAyNC45SyBWb3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00329345-qefkzgdjuq-portrait.jpg",
+        src: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/sapta-sagaradaache-ello--side-a-et00329345-1692362518.jpg",
         alt:"sapta sagaradaache ello" ,
         title:"Sapta Sagaradaache Ello" ,
         subtitle:"Drama, Romantic" 
     },
     {
-        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ni44LzEwICA0Ny44SyBWb3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00363916-xfaaanbjex-portrait.jpg" ,
+        src:"https://assets-in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/the-nun-ii-et00363916-1702637192.jpg" ,
         alt: "the nun 2",
         title:"The Nun 2" ,
         subtitle:"Horror, Mystery, Thriller" 
     },
     {
-        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OS8xMCAgNDA3LjVLIFZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00331686-tjdzkqhvev-portrait.jpg" ,
+        src:"https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/jailer-et00331686-1664432415.jpg" ,
         alt:"Jailer" ,
         title: "Jailer",
         subtitle: "Action, Drama, Thriller"
     },
     {
-        src: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny4xLzEwICAxMTEuMksgVm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00328665-alqlynvyqz-portrait.jpg",
+        src: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/kushi-et00328665-1693390326.jpg",
         alt: "Kushi",
         title: "Kushi",
         subtitle:"Comedy, Drama, Romantic" 
     },
     {
-        src: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC4yLzEwICA0MjMgVm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00368660-uktceyvnga-portrait.jpg",
+        src: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/baanadariyalli-et00368660-1693562365.jpg",
         alt:"Baanadariyalli" ,
         title:"Baanadariyalli" ,
         subtitle:"Adventure, Comedy, Romantic"
     },
     {
-        src:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC4xLzEwICA1LjlLIFZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00358212-bpaphjgyyb-portrait.jpg" ,
+        src:"https://assets-in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/the-equalizer-3-et00358212-1696502422.jpg" ,
         alt:"The Equalizer 3" ,
         title:"The Equalizer 3",
         subtitle:"Action, Crime,  Thriller" 
     },
     {
-        src: "https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OS4zLzEwICA1LjVLIFZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00370432-xebnbplscs-portrait.jpg",
+        src: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRjHTy7E4MJ0GbiwomY3xyfNh4aii-i0wrxM6PHCoW6gJquLqCl",
         alt:"Kannur Squad" ,
         title: "Kannur Squad",
         subtitle: "Action, Crime, Drama"
